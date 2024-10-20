8aa6f81093535b8813e97569cad5c28cbc4c97cb
kamal-sagar <136700810+kamal8882@users.noreply.github.com>
2024-10-20 12:30:51 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,20 @@
+let score = 325
+
+// console.log(score);
+// console.log(typeof score);
+
+let number = String(score);
+// console.log( number);
+// console.log(typeof number);
+
+let isLoggedIn = 1
+
+let conv = Boolean(isLoggedIn)
+// console.log(conv);
+// console.log(typeof conv);
+
+let name = "kamal"
+
+let number1 = Number(name)
+console.log(number1);
+console.log(typeof number1);
