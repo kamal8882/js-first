20e217e10ab3c5bef16bb12848cd8b1bb5d0ef74
kamal-sagar <136700810+kamal8882@users.noreply.github.com>
2024-10-20 10:51:53 +0000

    variables

--- basics/variable.js
+++ basics/variable.js
@@ -2,6 +2,7 @@
 const accountId = 1234;
 
 // "let" and "var" can be redefined;
+//  always use let instead of var bcze of block scope;
 let accountEmail = "kamal@google.com";
 var accountPass = "kamal12345";
 
@@ -11,7 +12,7 @@ accountEmail = "sagar@ms.com";
 accountPass = "sagar2345";
 accountCity = "delhi";
 
-console.log(accountId);
+console.log(accountId,accountEmail);
 // .table ([xyz, abc , efg....])
 console.table([accountEmail,accountPass,accountCity,accountId]);
     
