2947638f02b60ef7b05f86e2ae5a34716e5d713e
kamal-sagar <136700810+kamal8882@users.noreply.github.com>
2024-10-20 11:11:43 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+// datatypes;
+
+// string
+let name ="kamal";
+
+// number
+let age = 21;
+
+// boolean;
+let isLoggedIn = true /false ;
+
+// null => standalone value , null is not zero , it is an object
+
+// undefined => yet to define , it is undefined;
+
+// symbol => unique
+
+// you can check there type by "typeof" ;
+
+console.log(typeof age);
